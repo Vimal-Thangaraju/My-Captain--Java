d3550a81dc2c509cca5d57d9cf38fdd9131ca4a3
Vimal-Thangaraju <84367257+Vimal-Thangaraju@users.noreply.github.com>
2021-10-27 09:30:05 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee{
+
+public static void main (String args[])
+
+{
+
+System.out.println("Name \tYear of joining \tAddress");
+System.out.println();
+System.out.println("Robert \t1994 \t\t\t64C-WallsStreat");
+
+System.out.println("Sam \t2000 \t\t\t68D- WallsStreat");
+
+System.out.println("John \t1999 \t\t\t26B- WallsStreat");
+
+}
+
+}
